ee5ab370da0de9a08088a083290f33a2390bb279
DDoSolitary <DDoSolitary@gmail.com>
2017-05-20 18:19:01 +0800

    Add support for Surge configuration.

---
+++ generators/surge.js
@@ -0,0 +1,12 @@
+module.exports = {
+	file_name: "surge.conf",
+	generate: function (data) {
+		var generateEntry = function (ip, domain) {
+			return `${domain} = ${ip}`;
+		};
+		var generateComment = function (data) {
+			return data.replace(/^(.)/gm, '# $1');
+		};
+		return `[Host]\n\n${require('../generate-helper')(generateEntry, generateComment)(data)}`;
+	}
+};
