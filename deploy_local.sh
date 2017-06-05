2df31eaa06dc82f4985505a724703ecc30680dc3
lrinQVQ <cqlrin@gmail.com>
2017-06-06 03:46:29 +0800

    Add local generate shell

---
+++ deploy_local.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+if [ ! -f "$pwd/output" ]; then
+rm -rf output
+fi
+
+mkdir output
+node generate.js hosts.yml $(git show -s --format=%cd --date=short) output
+if [ "$PUSH_TO_MASTER" == "true" ]; then
+	echo $DEPLOY_KEY | base64 -d > ~/.ssh/id_ed25519
+	chmod 600 ~/.ssh/id_ed25519
+	git clone git@github.com:$TRAVIS_REPO_SLUG master
+	cp output/* master/
+	cd master
+	if [ -n "$(git status --porcelain)" ]; then
+		git add -A
+		git config user.name $(git show -s --format="%aN" $TRAVIS_COMMIT)
+		git config user.email $(git show -s --format="%aE" $TRAVIS_COMMIT)
+		git config push.default simple
+		{ printf "Update hosts from hosts-source.\n\n" & git log --format="%H %s" $TRAVIS_COMMIT_RANGE; } | GIT_COMMITTER_DATE=$(git show -s --format="%cD" $TRAVIS_COMMIT) git commit -F -
+		git push
+	fi
+fi
